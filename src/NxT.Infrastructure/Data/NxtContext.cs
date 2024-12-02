ae73387322f9c57d9f80a28adfe0486657087dc2
csvitor-dev <csvitor.dev@gmail.com>
2024-12-02 17:51:58 -0300

    feat('.Infrastructure'): implements data access context class (#2)

---
+++ src/NxT.Infrastructure/Data/NxtContext.cs
@@ -0,0 +1,15 @@
+using NxT.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace NxT.Infrastructure.Data;
+
+public class NxtContext
+    (DbContextOptions<NxtContext> options) : DbContext(options)
+{
+    public DbSet<Department> Departments { get; init; } = default!;
+    public DbSet<Seller> Sellers { get; init; } = default!;
+    public DbSet<SalesRecord> SalesRecords { get; init; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+        => modelBuilder.ApplyConfigurationsFromAssembly(typeof(NxtContext).Assembly);
+}
