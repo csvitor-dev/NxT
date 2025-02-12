9d22a7cad9aa24bb901910af781e5a35a008758f
Agnaldo Erick <erick.oliveira.git@gmail.com>
2025-02-12 16:21:39 -0300

    feat: apply migrations on running application

--- src/NxT.Infrastructure/Extensions/InfrastructureConfigExtension.cs
+++ src/NxT.Infrastructure/Extensions/InfrastructureConfigExtension.cs
@@ -1,9 +1,28 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NxT.Infrastructure.Data.Contexts;
 
 namespace NxT.Infrastructure.Extensions;
+
 
 public static class InfrastructureConfigExtension
 {
     public static string ConnectionString(this IConfiguration self)
         => self.GetConnectionString("db")!;
+
+    public static async Task ApplyMigrations(this IServiceCollection self)
+    {
+        using var scope = self.BuildServiceProvider().CreateScope();
+        var provider = scope.ServiceProvider;
+
+        try
+        {
+            await provider.GetService<NxtContext>()?.Database.MigrateAsync()!;
+        }
+        catch (System.Exception ex)
+        {
+            await Console.Error.WriteLineAsync($"Failed to migrate database.\n[{ex.Message}]");
+        }
+    }
 }
