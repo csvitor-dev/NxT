defbca6f6c6a8466f1d26bd14ce7d7c39a8bbdbf
csvitor-dev <csvitor.dev@gmail.com>
2025-02-13 21:09:34 -0300

    refactor: rename psql database provider

--- src/NxT.Infrastructure/Providers/PostgreSqlProvider.cs
+++ src/NxT.Infrastructure/Providers/PostgreSqlProvider.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using NxT.Infrastructure.Extensions;
 
 
 namespace NxT.Infrastructure.Providers;
 
-public class PostgrestProvider : IDbProvider
+public class PostgreSqlProvider : IDbProvider
 {
     public void Configure(DbContextOptionsBuilder options, IConfiguration configuration)
     {
